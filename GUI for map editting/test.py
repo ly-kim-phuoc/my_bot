1f7465b52ccc2d000ad5cc46ae4b5ea3927ec3b4
Kim Phuoc <lykimphuoc01@gmail.com>
2024-02-20 14:31:35 +0700

    Merge scripts and Python Files to main

---
+++ GUI for map editting/test.py
@@ -0,0 +1,3 @@
+import os
+os.system("/home/phuoc/Documents/Programming/Python/GUI\ for\ map\ editting/gazebo_script")
+# os.system("/home/phuoc/Documents/Programming/Python/GUI\ for\ map\ editting/rviz2_script")
