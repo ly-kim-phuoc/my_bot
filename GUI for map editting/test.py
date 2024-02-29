abd0158e02e362090fc9d2291ca70904697bca0f
Kim Phuoc <lykimphuoc01@gmail.com>
2024-02-29 14:15:54 +0700

    Rough exploration added

--- GUI for map editting/test.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("/home/phuoc/Documents/Programming/Python/GUI\ for\ map\ editting/gazebo_script")
-# os.system("/home/phuoc/Documents/Programming/Python/GUI\ for\ map\ editting/rviz2_script")
